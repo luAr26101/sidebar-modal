cb8e29d6a4765bf8cbef6f119d1eda5a0ea5de3a
luAr26101 <raul.cosmin.26@icloud.com>
2026-05-19 07:58:39 +0300

    Complete sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,46 @@
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "../context";
+import { links, social } from "../data";
+import logo from "../logo.svg";
+
 function Sidebar() {
-  return <>Sidebar</>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className={isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}>
+      <div className='sidebar-header'>
+        <img src={logo} alt='coding addict' className='logo' />
+        <button
+          type='button'
+          className='close-btn'
+          onClick={() => closeSidebar()}
+        >
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='links'>
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon} {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className='social-links'>
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 }
 
 export default Sidebar;
